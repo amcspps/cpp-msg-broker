2dc643a5e28ac81e254dddecfe385c69ff0756bf
amcspps <pashafizmat1@gmail.com>
2024-02-28 03:47:21 +0300

    client built

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,3 +1,6 @@
+/* 
+  server code
+*/
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,22 +16,24 @@
 int main(int argc, char const *const *argv) {
   char const *hostname;
   int port, status;
-  char const *exchange;
+  char const *exchangename;
+  char const *exchangetype;
   char const *bindingkey;
   amqp_socket_t *socket = NULL;
   amqp_connection_state_t conn;
 
   amqp_bytes_t queuename;
 
-  if (argc < 5) {
-    fprintf(stderr, "Usage: amqp_listen host port exchange bindingkey\n");
+  if (argc < 6) {
+    fprintf(stderr, "Usage: amqp_listen host port exchangename exchangetype bindingkey\n");
     return 1;
   }
 
   hostname = argv[1];
   port = atoi(argv[2]);
-  exchange = argv[3];
-  bindingkey = argv[4];
+  exchangename = argv[3];
+  exchangetype = argv[4];
+  bindingkey = argv[5];
 
   conn = amqp_new_connection();
 
@@ -47,8 +52,14 @@ int main(int argc, char const *const *argv) {
                     "Logging in");
   amqp_channel_open(conn, 1);
   die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
-
+  /*---------------------------------------------------------------------*/
   {
+    /*declaring an exchange (TODO: default type direct)*/
+    amqp_exchange_declare(conn, 1, amqp_cstring_bytes(exchangename),
+                        amqp_cstring_bytes(exchangetype), 0, 0, 0, 0,
+                        amqp_empty_table);
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring exchange");
+    /*declaring a queue*/
     amqp_queue_declare_ok_t *r = amqp_queue_declare(
         conn, 1, amqp_empty_bytes, 0, 0, 0, 1, amqp_empty_table);
     die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring queue");
@@ -57,16 +68,16 @@ int main(int argc, char const *const *argv) {
       fprintf(stderr, "Out of memory while copying queue name");
       return 1;
     }
-  }
-
-  amqp_queue_bind(conn, 1, queuename, amqp_cstring_bytes(exchange),
+    /*binding a queue to an exchange*/
+    amqp_queue_bind(conn, 1, queuename, amqp_cstring_bytes(exchangename),
                   amqp_cstring_bytes(bindingkey), amqp_empty_table);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Binding queue");
-
-  amqp_basic_consume(conn, 1, queuename, amqp_empty_bytes, 0, 1, 0,
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Binding queue");
+    /*consuming process startup*/
+    amqp_basic_consume(conn, 1, queuename, amqp_empty_bytes, 0, 1, 0,
                      amqp_empty_table);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Consuming");
-
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Consuming");
+  }  
+  /*----------------------------------------------------------------------*/
   {
     for (;;) {
       amqp_rpc_reply_t res;
@@ -80,7 +91,7 @@ int main(int argc, char const *const *argv) {
         break;
       }
 
-      printf("Delivery %u, exchange %.*s routingkey %.*s\n",
+      printf("Delivery %u, exchangename %.*s routingkey %.*s\n",
              (unsigned)envelope.delivery_tag, (int)envelope.exchange.len,
              (char *)envelope.exchange.bytes, (int)envelope.routing_key.len,
              (char *)envelope.routing_key.bytes);
@@ -93,6 +104,22 @@ int main(int argc, char const *const *argv) {
       printf("----\n");
 
       amqp_dump(envelope.message.body.bytes, envelope.message.body.len);
+
+      // Отправка ответа клиенту
+      amqp_basic_properties_t props;
+      props._flags = AMQP_BASIC_CONTENT_TYPE_FLAG |
+                     AMQP_BASIC_DELIVERY_MODE_FLAG;
+      props.content_type = amqp_cstring_bytes("text/plain");
+      props.delivery_mode = 2; // persistent delivery mode
+
+      // Публикация сообщения в тот же exchangename и с тем же routing key
+      die_on_error(amqp_basic_publish(conn, 1, amqp_cstring_bytes(exchangename),
+                                      amqp_cstring_bytes(bindingkey), 0, 0,
+                                      &props, amqp_cstring_bytes("привет")),
+                   "Publishing");
+
+      amqp_destroy_envelope(&envelope);
+
 
       amqp_destroy_envelope(&envelope);
     }
